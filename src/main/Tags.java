17e7a42155f97bdd00adbf215d76946ecc2dce9e
dfdvdsf <81430928+song682@users.noreply.github.com>
2026-05-30 12:35:13 +0800

    Delete src/main directory

--- src/main/Tags.java
+++
@@ -1,8 +0,0 @@
-package decok.dfcdvadstf.soundswitcher;
-public class Tags {
-    public static final String MODID = "soundswitcher";
-    public static final String NAME = "Sound Switcher";
-    public static final String VERSION = "1.0.1";
-    public static final String ACCEPTED_VERSION = "1.7.10";
-
-}
